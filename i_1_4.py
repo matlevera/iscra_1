f60601589bcf2e66f76fcd498b39cc1b5524bad1
matlevera <100791350+matlevera@users.noreply.github.com>
2023-03-21 00:14:10 +0300

    Add files via upload

---
+++ i_1_4.py
@@ -0,0 +1,7 @@
+from math import fsum
+line = input()
+numbers = line.split()
+result = [float(num) for num in numbers]
+powresult = [pow(res, 2) for res in result]
+sumresult = [sum(powresult)]
+print(sumresult)
